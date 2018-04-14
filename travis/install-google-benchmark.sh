19f946cb176f8d96cbe3f024c3823258012fdc98
Adam Blaszkiewicz <atablash@atablash.pl>
2018-04-14 08:32:58 +0200

    ...

--- travis/install-google-benchmark.sh
+++ travis/install-google-benchmark.sh
@@ -11,11 +11,8 @@ rm -rf $DIR && mkdir $DIR && cd $DIR
 git clone https://github.com/google/benchmark.git google-benchmark --depth 1
 
 ( cd google-benchmark && \
-	eval cmake . \
-	-DBENCHMARK_ENABLE_GTEST_TESTS=OFF \
-	-DCMAKE_BUILD_TYPE=RELEASE \
-	$1 \
- 	&& make -j8 && make install DESTDIR=$PREFIX )
+	eval cmake . -DBENCHMARK_ENABLE_GTEST_TESTS=OFF -DCMAKE_BUILD_TYPE=RELEASE $1 \
+ 	&& make -j8 VERBOSE=1 && make install DESTDIR=$PREFIX )
 
 
 
