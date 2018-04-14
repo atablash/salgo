95536af52e9c3cbb943fcc5a0b9d3adc7b8bc4bd
Adam Blaszkiewicz <atablash@atablash.pl>
2018-04-14 05:14:39 +0200

    ...

--- travis/install-libcxx.sh
+++ travis/install-libcxx.sh
@@ -13,12 +13,12 @@ svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx       >/dev/null
 svn co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi >/dev/null
 
 # build libc++abi
-( cd libcxxabi && mkdir build && cd build && export CC=clang-5.0 CXX=clang++-5.0 && \
+( cd libcxxabi && mkdir build && cd build && export CC=clang CXX=clang++ && \
 	cmake -DLIBCXXABI_LIBCXX_INCLUDES=../../libcxx/include \
 	.. && make -j8 && make install --prefix $PREFIX )
 
 # build libc++
-( mkdir build && cd build && export CC=clang-5.0 CXX=clang++-5.0 && \
+( mkdir build && cd build && export CC=clang CXX=clang++ && \
 	cmake -DLLVM_PATH=../llvm \
 	-DLIBCXX_CXX_ABI=libcxxabi \
 	-DLIBCXX_CXX_ABI_INCLUDE_PATHS=../libcxxabi/include \
